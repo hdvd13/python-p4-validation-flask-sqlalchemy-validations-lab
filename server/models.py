5893a9b769c242396b6d0c91fc68d70a561acab4
Hossein <h.dvd13@gmail.com>
2023-09-17 16:04:42 -0700

    update2

--- server/models.py
+++ server/models.py
@@ -1,38 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
-
 db = SQLAlchemy()
 
-
 class Author(db.Model):
-    __tablename__ = "authors"
-    # Add validations and constraints
+    __tablename__ = 'authors'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    name= db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    @validates("name")
-    def validate_name(self, key, name):
-        if not name:
-            raise ValueError("Author must have name")
-        return name
-
-    @validates("phone_number")
-    def validate_PN(self, key, pn):
-        if len(pn) != 10:
-            raise ValueError("Phone Number must be 10 digits")
-        return pn
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())    
 
     def __repr__(self):
-        return f"Author(id={self.id}, name={self.name})"
-
+        return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError("Name field is required.")
+        elif name in names:
+            raise ValueError("Name must be unique.")
+        return name 
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, address):
+        if len(address) != 10:
+            raise ValueError("Phone number must be 10 digits")
+        return address
 
 class Post(db.Model):
-    __tablename__ = "posts"
-    # Add validations and constraints
+    __tablename__ = 'posts'
+    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -42,39 +41,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    @validates("title")
-    def validate_title(self, key, title):
-        if not title:
-            raise ValueError("Title cannot be empty")
+    @validates('title')
+    def validate_title(self, key, address):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in address for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return address
 
-        strings = ["Won't Believe", "Secret", "Top", "Guess"]
-        num = 0
-        for str in strings:
-            if title.find(str) != -1:
-                num += 1
-        if num < 4:
-            raise ValueError("not clickbaity enough")
+    @validates('content')
+    def validate_content(self, key, address):
+        if len(address) < 250:
+            raise ValueError("failed content length validation")
+        return address
+    
+    @validates('summary')
+    def validate_summary(self, key, address):
+        if len(address) >= 250:
+            raise ValueError("failed summary length validation")
+        return address
+    
+    @validates('category')
+    def validate_category(self, key, address):
+        if address != 'Fiction' and address != 'Non-Fiction':
+            raise ValueError("failed category validation")
+        return address
 
-        return title
-
-    @validates("content")
-    def validate_content(self, key, cont):
-        if not len(cont) >= 250:
-            raise ValueError("Post content must be 250 characters or more")
-        return cont
-
-    @validates("summary")
-    def validate_summary(self, key, summ):
-        if len(summ) >= 250:
-            raise ValueError("Summary cannot be greater than 250 characters")
-        return summ
-
-    @validates("category")
-    def validate_category(self, key, cat):
-        cats = ["Fiction", "Non-Fiction"]
-        if cat not in cats:
-            raise ValueError("Category must be Fiction or Non-Fiction")
-        return cat
 
     def __repr__(self):
-        return f"Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})"
+        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
